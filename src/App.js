b6b6f20d7ba4794ef9cb0b99f967d99d93ef20e6
MR-VSR <vikasraivsr81@gmail.com>
2022-07-11 22:01:46 +0530

    Navbar and footer added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Career from "./components/Career.js";
+import Navbar from "./components/Navbar.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<BrowserRouter>
+      
+        <Navbar/>
+      <Routes>
+        <Route exact path="/career"  element={<Career/>}/>
+
+
+      </Routes>
+        <Footer />
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/Career.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function Career() {
+  return (
+    <>
+      <h1>This is the Career page</h1>
+    </>
+    // <Navbar />
+  )
+}
+
+export default Career
